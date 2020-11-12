4b0e4ea613316a88072f190d7ffef944337928da
dwandrew <danwandrew@gmail.com>
2020-11-12 14:14:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function findMinAndRemoveSorted(array){
+    let min = Math.min(...array)
+    let index = array.indexOf(min)
+    array.splice(index, 1)
+    return min
+}
 
+function merge(array1, array2){
+    let len = array1.length + array2.length
+    let combined = array1.concat(array2)
+    let sorted = []
+    while (sorted.length < len){
+        let num = findMinAndRemoveSorted(combined)
+        sorted.push(num)
+    } 
+    return sorted
+}
+
+function mergeSort(array){
+    let len = array.length
+    let sorted = []
+    while (sorted.length < len){
+        let num = findMinAndRemoveSorted(array)
+        sorted.push(num)
+    } 
+    return sorted
+}
